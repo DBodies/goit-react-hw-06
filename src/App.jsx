83ffbb11113f7fd318e29194909302aa0818acfc
Denis <159953887+DBodies@users.noreply.github.com>
2025-05-22 15:04:18 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import initialPhoneList from './phoneList.json'
+import ContactList from './components/contactList'
+import SearchBox from './components/searchBox'
+import ContactForm from './components/contactForm'
+import styles from './components/stylesForAllComponents.module.css'
+
+export default function App() {
+
+  const [phoneList, setPhoneList] = useState(() => {
+    const saved = localStorage.getItem("saved-contact")
+    if (saved) {
+      return JSON.parse(saved)
+    }
+   return initialPhoneList
+  }) 
+  
+useEffect(() => {
+  localStorage.setItem("saved-contact", JSON.stringify(phoneList));
+}, [phoneList]);
+
+  const [filter, setFilter] = useState(() => {
+    const saved = localStorage.getItem("saved-filter")
+    if (saved) {
+      return JSON.parse(saved)
+    }
+    return ""
+  })
+
+  const visibleFilter = phoneList.filter(phoneListItem => phoneListItem.name.toLowerCase().includes(filter.toLowerCase()))
+  
+  const addContact = (newContact) => {
+    setPhoneList((prevContact) => {
+      return [...prevContact, newContact]
+    })
+  }
+
+  const deleteContact = (contactId) => {
+    setPhoneList((prevContact) => {
+  return prevContact.filter((currentContact) => currentContact.id !== contactId)
+})
+  }
+
+  const resetContacts = () => {
+    setPhoneList(initialPhoneList);
+    localStorage.removeItem('saved-contact'); 
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={ addContact} />
+      <SearchBox value={ filter} onFilter={setFilter} />
+      <ContactList phoneList={visibleFilter} onDelete={deleteContact} />
+      <button onClick={resetContacts}>return Contacts</button>
+    </div>
+  )
+}
+
+
---
+++ src/components/contacts.jsx
@@ -0,0 +1,12 @@
+
+export default function Contacts({name, number, onDelete, id}) {
+    
+
+    return (
+        <>
+            <p>{name}</p>
+            <p>{number}</p>
+            <button onClick={() => onDelete(id)}>Delete</button>
+        </>
+    )
+}
